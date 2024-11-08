1fe05b712014b99bc612096bba23fe24f9a0d3b9
Ryan <6885827+rtfee@users.noreply.github.com>
2024-11-08 12:35:23 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,3 +3,9 @@ resource "null_resource" "example" {
     command = "echo 'Demo of a run from a PR comment!!'"
   }
 }
+
+resource "null_resource" "example2" {
+  provisioner "local-exec" {
+    command = "echo 'Demo of a run from a PR comment!!'"
+  }
+}
