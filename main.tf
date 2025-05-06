4910a3ed0005fe2bc2cf72fbd403d145c5387b82
Ryan <6885827+rtfee@users.noreply.github.com>
2025-05-06 10:36:21 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,3 +7,4 @@ module "s3-bucket" {
 	# (Optional, Forces new resource) The name of the bucket. If omitted, Terraform will assign a random, unique name.
  	bucket = var.bucketname
 }
+breaking change
